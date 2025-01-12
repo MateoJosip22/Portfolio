cbef0193e83af1fff29d1f0629e2915610f4cf77
Mateo Josipovic <mateoj2201@gmail.com>
2025-01-12 03:56:39 +0100

    rename Java project1 to GTN

--- Coding_Languages/Java/GuessTheNumber/GTN.java
+++ Coding_Languages/Java/GuessTheNumber/GTN.java
@@ -1,7 +1,8 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class GuessTheNumber {
+public class GTN {
+    @SuppressWarnings("ConvertToTryWithResources")
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
