d847d176e83ef12e7b1ccf229993bdbdd9082503
Mateo Josipovic <mateoj2201@gmail.com>
2025-01-12 03:46:08 +0100

    Python project 1 and Java project 1 added

---
+++ Coding_Languages/Java/GuessTheNumber/GuessTheNumber.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessTheNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int numberToGuess = random.nextInt(100) + 1;
+        int userGuess = 0;
+
+        System.out.println("Welcome to the Guess the Number game!");
+        System.out.println("I have selected a number between 1 and 100. Can you guess it?");
+
+        while (userGuess != numberToGuess) {
+            System.out.print("Enter your guess: ");
+            userGuess = scanner.nextInt();
+
+            if (userGuess < numberToGuess) {
+                System.out.println("Too low! Try again.");
+            } else if (userGuess > numberToGuess) {
+                System.out.println("Too high! Try again.");
+            } else {
+                System.out.println("Congratulations! You guessed the number!");
+            }
+        }
+        scanner.close();
+    }
+}
